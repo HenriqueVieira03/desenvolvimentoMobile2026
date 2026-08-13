2802baba8805684ead5932a70cd87a76c874c3c5
Henrique Vieira <henrivira14@outlook.com>
2026-08-12 22:10:36 -0300

    Adicionado Aplicativo de Calcular Distância entre dois pontos

---
+++ Distance/Distance/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace Distance
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btCalcularDistancia_Clicked(object sender, EventArgs e)
+        {
+            double ax = Convert.ToDouble(Ax.Text);
+            double ay = Convert.ToDouble(Ay.Text);
+            double az = Convert.ToDouble(Az.Text);
+
+            double bx = Convert.ToDouble(Bx.Text);
+            double by = Convert.ToDouble(By.Text);
+            double bz = Convert.ToDouble(Bz.Text);
+
+            double distance = Math.Sqrt(Math.Pow(bx - ax, 2) + Math.Pow(by - ay, 2) + Math.Pow(bz - az, 2));
+
+            lbResultado.Text = $"{distance:F4}";
+            ;
+        }
+    }
+}
