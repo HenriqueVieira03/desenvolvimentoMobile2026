ad7a3965b7aea995d40039b7ae13369b0549c700
Henrique Vieira <henrivira14@outlook.com>
2026-09-17 22:54:26 -0300

    Adiciona Styles.xaml com estilos personalizados MAUI
    Arquivo Styles.xaml criado com dicionário de recursos XAML contendo estilos personalizados para ActivityIndicator, Button, Label, Entry, Switch e outros controles do .NET MAUI. Estilos definem propriedades visuais e estados (Normal, Disabled, PointerOver), com suporte a temas claro e escuro via AppThemeBinding. Inclui estilos para páginas, Shell, NavigationPage e TabbedPage, além de exemplos comentados para TitleBar.

---
+++ Checkboxes/Checkboxes/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace Checkboxes
+{
+    public partial class MainPage : ContentPage
+    {
+        public float fTotal;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            fTotal = 0.0f;
+            lbTotal.Text = $"{fTotal:F2}";
+        }
+
+        private void Checkbox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            fTotal = Convert.ToSingle(lbTotal.Text);
+
+            CheckBox chk = (CheckBox)sender;
+
+            if (chk.Equals(chk1))
+            {
+                if (chk.IsChecked)
+                    fTotal += Convert.ToSingle(val1.Text);
+                else
+                    fTotal -= Convert.ToSingle(val1.Text);
+            }
+            else if (chk.Equals(chk2))
+            {
+                if (chk.IsChecked)
+                    fTotal += Convert.ToSingle(val2.Text);
+                else
+                    fTotal -= Convert.ToSingle(val2.Text);
+            }
+            else if (chk.Equals(chk3))
+            {
+                if (chk.IsChecked)
+                    fTotal += Convert.ToSingle(val3.Text);
+                else
+                    fTotal -= Convert.ToSingle(val3.Text);
+            }
+
+            lbTotal.Text = $"{fTotal:F2}";
+        }
+
+
+    }
+}
